5bca198393178712ce698a7e9637d4506d1e6003
Wong Ming Yeung <marcus2288@hotmail.com>
2023-10-05 15:40:25 +0800

    start backend npm init

---
+++ back-end/knexfile.ts
@@ -0,0 +1,28 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+module.exports = {
+  development: {
+    client: "postgresql",
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: "knex_migrations",
+    },
+  },
+
+  staging: {
+    //...
+  },
+
+  production: {
+    //...
+  },
+};
