937ef3847a7acc254d917a5598d66dd87af75a00
Wong Ming Yeung <marcus2288@hotmail.com>
2023-10-05 15:55:16 +0800

    createdb job

--- back-end/knexfile.ts
+++ back-end/knexfile.ts
@@ -1,9 +1,11 @@
-import dotenv from "dotenv";
-dotenv.config();
+import type { Knex } from 'knex'
+import dotenv from 'dotenv'
 
-module.exports = {
+// Update with your config settings.
+dotenv.config()
+const config: { [key: string]: Knex.Config } = {
   development: {
-    client: "postgresql",
+    client: 'postgresql',
     connection: {
       database: process.env.DB_NAME,
       user: process.env.DB_USERNAME,
@@ -14,15 +16,42 @@ module.exports = {
       max: 10,
     },
     migrations: {
-      tableName: "knex_migrations",
+      tableName: 'knex_migrations',
     },
+    debug: true,
   },
 
   staging: {
-    //...
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
   },
 
   production: {
-    //...
+    client: 'postgresql',
+    connection: {
+      database: process.env.DB_NAME,
+      user: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      tableName: 'knex_migrations',
+    },
   },
-};
+}
+
+module.exports = config
