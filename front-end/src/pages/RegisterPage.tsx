d159596ba4a1a74cff9bfb18a0d6141cfa2c7158
Wong Ming Yeung <marcus2288@hotmail.com>
2023-10-06 18:07:16 +0800

    update register

--- front-end/src/pages/RegisterPage.tsx
+++ front-end/src/pages/RegisterPage.tsx
@@ -13,9 +13,48 @@ import {
   useIonRouter,
 } from "@ionic/react";
 import { Link } from "react-router-dom";
-
+import { useMaskito } from "@maskito/react";
 const RegisterPage: React.FC = () => {
   const title = "Register";
+
+  const cardMask = useMaskito({
+    options: {
+      mask: [
+        ...Array(4).fill(/\d/),
+        " ",
+        ...Array(4).fill(/\d/),
+        " ",
+        ...Array(4).fill(/\d/),
+        " ",
+        ...Array(4).fill(/\d/),
+        " ",
+        ...Array(3).fill(/\d/),
+      ],
+    },
+  });
+
+  const phoneMask = useMaskito({
+    options: {
+      mask: [
+        "+",
+        "(",
+        "8",
+        "5",
+        "2",
+        ")",
+        " ",
+        /\d/,
+        /\d/,
+        /\d/,
+        /\d/,
+        "-",
+        /\d/,
+        /\d/,
+        /\d/,
+        /\d/,
+      ],
+    },
+  });
 
   return (
     <IonPage>
@@ -43,6 +82,30 @@ const RegisterPage: React.FC = () => {
           </IonItem>
           <IonItem>
             <IonInput type="number" label="HKID" />
+          </IonItem>
+          <IonItem>
+            <IonInput
+              ref={async (cardRef) => {
+                if (cardRef) {
+                  const input = await cardRef.getInputElement();
+                  cardMask(input);
+                }
+              }}
+              label="Card number"
+              placeholder="0000 0000 0000 0000"
+            ></IonInput>
+          </IonItem>
+          <IonItem>
+            <IonInput
+              ref={async (phoneInput) => {
+                if (phoneInput) {
+                  const input = await phoneInput.getInputElement();
+                  phoneMask(input);
+                }
+              }}
+              label="HK phone number"
+              placeholder="+(852) xxxx-xxxx"
+            ></IonInput>
           </IonItem>
         </IonList>
         <IonButton routerLink="/drawKey">SignUp</IonButton>
